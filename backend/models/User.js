2100cd3d56ac41450cc017fd1b320816a81b83c8
Rich Hall <rjhall@gmail.com>
2024-04-04 07:45:38 +0000

    mongoose again

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,7 @@ var UserSchema = new mongoose.Schema(
       enum: ["user", "admin"],
       default: "user"
     },
-    isVerified: Boolean,
+    isVerified: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
@@ -76,8 +76,7 @@ UserSchema.methods.toAuthJSON = function() {
     token: this.generateJWT(),
     bio: this.bio,
     image: this.image,
-    role: this.role,
-    isVerified: this.isVerified
+    role: this.role
   };
 };
 
